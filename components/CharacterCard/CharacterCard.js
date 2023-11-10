9f487b0db14cc05326d6ad5ab145313a047feb45
Alexander Kolotow <alexkolotow@gmail.com>
2023-11-10 12:12:00 +0100

    update index.js

--- components/CharacterCard/CharacterCard.js
+++ components/CharacterCard/CharacterCard.js
@@ -1,11 +1,8 @@
 export function CharacterCard(character) {
 
-  const cardContainer = document.createElement('ul');
-  cardContainer.classList.add('card-container');
-  cardContainer.setAttribute('data-js', 'card-container');
   const card = document.createElement('li');
   card.classList.add('card');
-  card.innerHTML = `<div class="card__image-container">
+  /*card.innerHTML = `<div class="card__image-container">
             <img
               class="card__image"
               src="${character.image}"
@@ -23,8 +20,29 @@ export function CharacterCard(character) {
               <dt class="card__info-title">Occurrences</dt>
               <dd class="card__info-description">${character.occurrences}</dd>
             </dl>
-          </div>`;
-  // cardContainer.appendChild(card);
-  document.body.append(card);
-  return cardContainer;
+          </div>`;*/
+          card.innerHTML = ` <div class="card__image-container">
+          <img
+            class="card__image"
+            src="https://rickandmortyapi.com/api/character/avatar/1.jpeg"
+            alt="Rick Sanchez"
+          />
+          <div class="card__image-gradient"></div>
+        </div>
+        <div class="card__content">
+          <h2 class="card__title">Rick Sanchez</h2>
+          <dl class="card__info">
+            <dt class="card__info-title">Status</dt>
+            <dd class="card__info-description">Alive</dd>
+            <dt class="card__info-title">Type</dt>
+            <dd class="card__info-description"></dd>
+            <dt class="card__info-title">Occurrences</dt>
+            <dd class="card__info-description">51</dd>
+          </dl>
+        </div>
+          
+          `;
+  //cardContainer.appendChild(card);
+  //document.body.append(card);
+  return card;
 }
--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+import { CharacterCard } from "./components/CharacterCard/CharacterCard.js";
+
+
 const cardContainer = document.querySelector('[data-js="card-container"]');
 const searchBarContainer = document.querySelector(
   '[data-js="search-bar-container"]'
@@ -12,3 +15,6 @@ const pagination = document.querySelector('[data-js="pagination"]');
 const maxPage = 1;
 const page = 1;
 const searchQuery = "";
+
+const card = CharacterCard();
+cardContainer.append(card);
